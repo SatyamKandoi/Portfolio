bff1879c0c3587ea5c7378599da6e95cd9c411e2
Satyam Kandoi <satyam.19beceg003@gmail.com>
2023-04-13 04:24:51 +0530

    Update CTA.jsx

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ const CTA = () => {
   return (
     <div className='cta'>
     <a href={CV} download className='btn'>Download CV</a>
-    <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+    <a href="https://drive.google.com/file/d/1Wc4LFpCsnChApPZhYRtB3O-yU9Ri7Hyp/view?usp=sharing" className='btn btn-primary'>Let's Talk</a>
 
     </div>
   )
