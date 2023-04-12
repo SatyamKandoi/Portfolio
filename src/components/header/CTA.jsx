6a2a67bb9e3f6e19f88b4744e4ec4cb5b6266b77
Satyam Kandoi <satyam.19beceg003@gmail.com>
2023-04-13 04:27:33 +0530

    Update CTA.jsx

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,7 @@ import CV from '../../assets/cv.jpg'
 const CTA = () => {
   return (
     <div className='cta'>
-    <a href={https://drive.google.com/file/d/1Wc4LFpCsnChApPZhYRtB3O-yU9Ri7Hyp/view?usp=sharing} download className='btn'>Download CV</a>
+    <a href="https://drive.google.com/file/d/1Wc4LFpCsnChApPZhYRtB3O-yU9Ri7Hyp/view?usp=sharing" download className='btn'>Download CV</a>
     <a href="#contact" className='btn btn-primary'>Let's Talk</a>
 
     </div>
